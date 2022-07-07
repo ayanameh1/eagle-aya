694b69021d4395a7eeab53badd7411e3fc9cfdfc
Edma Abboud <abboudEdma@gmail.com>
2022-07-07 14:37:52 +0300

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:eagle/ui/add_expo.dart';
 import 'package:eagle/ui/homelayout.dart';
 import 'package:eagle/ui/homepage.dart';
 import 'package:eagle/ui/login.dart';
@@ -30,7 +31,7 @@ class MyApp extends StatelessWidget {
           ),
         ),
       ),
-      home: HomeLayout(),
+      home: AddExpoScreen(),
     );
   }
 }
