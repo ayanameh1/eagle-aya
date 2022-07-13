c57e4bed16c33d56a86bb6d44f739273e1db50ce
Edma Abboud <abboudEdma@gmail.com>
2022-07-13 23:35:15 +0300

    multiple image picker done

---
+++ lib/CN/pick_multiple_images.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+import 'package:image_picker/image_picker.dart';
+
+class MultiImages extends ChangeNotifier{
+  int ?indexx;
+  final ImagePicker imagePicker = ImagePicker();
+  List<XFile?>? imageFileList = [];
+
+
+  //
+  Future selectImages() async {
+    final List<XFile>? selectedImages = await imagePicker.pickMultiImage();
+    if (selectedImages!.isNotEmpty) {
+      imageFileList!.addAll(selectedImages);
+      notifyListeners();
+    }}
+
+
+    //
+  Future deleteimage(indexx, imageFileList) async {
+      await imageFileList.removeAt(indexx);
+      print(imageFileList.length);
+      notifyListeners();
+    }
+
+}
