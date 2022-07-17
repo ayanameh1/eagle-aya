5dfc7c7fae4ee6dcffe74fca48e2b0458c0884d8
Edma Abboud <abboudEdma@gmail.com>
2022-07-17 14:56:46 +0300

    firebase messaging 4

---
+++ lib/services/local_notificatios_services.dart
@@ -0,0 +1,31 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+class LocalNotificationServices {
+  static final FlutterLocalNotificationsPlugin _notificationsPlugin =
+  FlutterLocalNotificationsPlugin();
+
+  static void initialize() {
+    final InitializationSettings initializationSettings =
+    InitializationSettings(
+        android: AndroidInitializationSettings("@mipmap/ic_launcher"));
+    _notificationsPlugin.initialize(initializationSettings);
+  }
+
+  static void display(RemoteMessage message) async {
+    try {
+      final id = DateTime
+          .now()
+          .microsecondsSinceEpoch ~/ 1000;
+      final NotificationDetails notificationDetails = NotificationDetails(
+          android: AndroidNotificationDetails(
+              'easyapproach', 'easyapproach channel',
+              importance: Importance.max, priority: Priority.high));
+
+      await _notificationsPlugin.show(id, message.notification!.title,
+          message.notification!.title, notificationDetails);
+    }on Exception catch(e){
+      print(e);
+  }
+}
+}
